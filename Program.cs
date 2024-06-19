31738557d854168014e5516ee0a1083d38a05447
AbdulHannan3333 <hannan67212@outlook.com>
2024-06-19 20:36:36 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9._2_Function_with_parameters_and_return_value
+{
+    internal class Program
+    {
+        static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        static void Main(string[] args)
+        {
+            int Result = Add(5, 10);
+            Console.WriteLine($"Result of x and y {Result}");
+        }
+    }
+}
